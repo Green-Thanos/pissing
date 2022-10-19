1fc510c69fa7caec653a7bd3dc060fa8f7f139b6
hurb <53662042+Green-Thanos@users.noreply.github.com>
2022-10-18 22:51:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+export function leak(mb) {
+    if (typeof window === "undefined") {
+        global.piss = new Uint8Array(Math.floor(mb * 1024 * 1024)).fill(69);
+    } else {
+        window.piss = new Uint8Array(Math.floor(mb * 1024 * 1024)).fill(69);
+    }
+}
